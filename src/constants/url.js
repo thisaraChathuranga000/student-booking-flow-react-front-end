3218c6b039741f501fc5a3c83958b27332b58e5d
Thisara Chathuranga <thisarachathuranga000@gmail.com>
2025-09-07 23:52:12 +0530

    Update BASE_URL to production backend endpoint
    Changed BASE_URL from localhost to the deployed backend on Render for production use.

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,2 +1,2 @@
 export const INVITE_URL = "https://mail.google.com/mail/u/0/#inbox";
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://student-booking-flow-nestjs-back-end-2.onrender.com";
