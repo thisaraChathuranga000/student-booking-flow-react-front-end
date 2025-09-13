916efed3edc35f24e4378d3f933699116aea3e72
Thisara Chathuranga <thisarachathuranga000@gmail.com>
2025-09-14 00:09:51 +0530

    Switch to HashRouter for routing
    Replaces BrowserRouter with HashRouter in App.js to improve compatibility with static file hosting environments that do not support server-side routing.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from "./store";
